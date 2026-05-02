3e6ad676ad735bf834e504c8eb97f20e8336bd2a
Busra Yagcioglu <busrayagcioglu2003@gmail.com>
2026-05-03 00:46:24 +0300

    Configure Supabase Postgres as Django default database

--- core/settings.py
+++ core/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import dj_database_url
 
 load_dotenv()
 
@@ -90,12 +91,27 @@ TEMPLATES = [
 WSGI_APPLICATION = "core.wsgi.application"
 
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=not DEBUG,
+        )
     }
-}
+else:
+    if DEBUG:
+        DATABASES = {
+            "default": {
+                "ENGINE": "django.db.backends.sqlite3",
+                "NAME": BASE_DIR / "db.sqlite3",
+            }
+        }
+    else:
+        raise ValueError("DEBUG=False iken DATABASE_URL .env içinde tanımlı olmalı.")
 
 
 LANGUAGE_CODE = "tr-tr"
